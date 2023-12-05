5aa2ccb899ce4b1a688e0af24b37d1ca10b4fced
Silence Chibarara <silence.chibarara@cassavasmartech.co.zw>
2023-12-05 15:40:49 +0200

    inject dispatcher

--- app/src/main/java/sc/fruity/repository/FruityRepository.kt
+++ app/src/main/java/sc/fruity/repository/FruityRepository.kt
@@ -1,19 +1,21 @@
 package sc.fruity.repository
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import sc.fruity.data.Fruit
 import sc.fruity.repository.remote.RetrofitFruityApi
 import sc.fruity.repository.remote.WebService
 
-class FruityRepository(private val fruityWebService: WebService) {
+class FruityRepository(private val fruityWebService: WebService, private val defaultDispatcher: CoroutineDispatcher = Dispatchers.IO
+) {
     companion object {
         const val BASE_URL = "https://www.fruityvice.com/api/fruit/"
     }
 
 
     suspend fun getAll(): List<Fruit>
-            = withContext(Dispatchers.IO) {
+            = withContext(defaultDispatcher) {
         return@withContext fruityWebService.getAll()
     }
 
